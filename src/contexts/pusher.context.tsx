b7e5a1649085220eed08477d9a8ec3f26064a331
Toanf2103 <nobitakute002@gmail.com>
2024-10-06 14:07:37 +0700

    config pusher

---
+++ src/contexts/pusher.context.tsx
@@ -0,0 +1,39 @@
+import { LayoutProps } from '@/types'
+import { createContext, useContext, useEffect, useMemo } from 'react'
+import Pusher from 'pusher-js'
+import { getEnv } from '@/helpers'
+
+type TPusherContext = {}
+
+const PusherContext = createContext<TPusherContext | undefined>(undefined)
+
+export const usePusher = (): TPusherContext => {
+  const context = useContext(PusherContext)
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
+
+export const PuhserProvider = (props: LayoutProps) => {
+  const { children } = props
+  const pusher = useMemo(() => {
+    return new Pusher(getEnv('VITE_PUSHER_APP_KEY'), {
+      cluster: getEnv('VITE_PUSHER_APP_CLUSTER'),
+    })
+  }, [])
+  useEffect(() => {
+    const channel = pusher.subscribe('toanf')
+    channel.bind('toanf', function (data: any) {
+      console.log(JSON.stringify(data))
+    })
+
+    return () => {
+      pusher.unsubscribe('toanf')
+    }
+  }, [])
+
+  const contextValue: TPusherContext = {}
+
+  return <PusherContext.Provider value={contextValue}>{children}</PusherContext.Provider>
+}
