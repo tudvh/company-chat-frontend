fe0fcaa01e76c3bc72ed2c4f6888ba1b0fa37f86
tudvh <dangtu9520@gmail.com>
2024-10-27 15:39:51 +0700

    fix: conflict

--- src/pages/Channel/JoinChannel.tsx
+++ src/pages/Channel/JoinChannel.tsx
@@ -1,6 +1,6 @@
 import { ROUTES } from '@/configs'
 import { useAuth } from '@/contexts'
-import { useJointedChannels } from '@/hooks'
+import { useChannels } from '@/hooks'
 import { ChannelService } from '@/services/api'
 import { ToastUtil } from '@/utils'
 import { useEffect } from 'react'
@@ -10,7 +10,7 @@ export const JoinChannel = () => {
   const { code } = useParams()
   const navigate = useNavigate()
   const { userProfile } = useAuth()
-  const { refresh } = useJointedChannels(userProfile?.id)
+  const { invalidateChannels } = useChannels(userProfile?.id)
 
   const joinChannel = async (code: string) => {
     const channel = await ChannelService.joinChannel(code)
@@ -18,7 +18,7 @@ export const JoinChannel = () => {
     if (!channel) {
       return navigate(ROUTES.HOME)
     }
-    refresh()
+    invalidateChannels()
     ToastUtil.success('Tham gia máy chủ thành công')
     navigate(ROUTES.CHANNEL.replace(':channelId', channel.id))
   }
